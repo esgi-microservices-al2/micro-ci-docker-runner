72e1c40314ca78157d89ed60b2206dc74e230338
N-injin <jimbienvenu@gmail.com>
2020-07-14 21:57:07 +0200

    Swap fmt to log

--- functions.go
+++ functions.go
@@ -5,7 +5,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -111,7 +110,7 @@ func RemoveEncyptionFromID(id string) string {
 
 // SendEventMessage ... Sends a message to the rabbitMQ event queue
 func SendEventMessage(eventMessage EventMessage, channel *amqp.Channel, queue string) {
-	fmt.Printf("Sending to events : %+v", eventMessage)
+	log.Printf("Sending to events : %+v", eventMessage)
 	body, err := json.Marshal(eventMessage)
 	if err != nil {
 		log.Printf(err.Error())
